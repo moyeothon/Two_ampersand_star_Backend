c9e4142ad240af0a4a9498a241750a0936234630
GamJaDo <kim0615157@gmail.com>
2024-10-29 02:05:49 +0900

    init

---
+++ src/main/java/moyeothon/Team11_TwoAmpersandStar/PyapyatApplication.java
@@ -0,0 +1,14 @@
+package moyeothon.Team11_TwoAmpersandStar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyapyatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyapyatApplication.class, args);
+	}
+
+}
+git 
---
+++ src/test/java/moyeothon/Team11_TwoAmpersandStar/PyapyatApplicationTests.java
@@ -0,0 +1,13 @@
+package moyeothon.Team11_TwoAmpersandStar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyapyatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
