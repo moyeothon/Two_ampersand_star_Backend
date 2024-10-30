6ec9506e390ff70abfb322a4a93b4a435c96ea96
ckr8305 <ckr8305@naver.com>
2024-10-30 16:27:15 +0900

    refactor: 시큐리티, 트랜잭션 제외

--- src/main/java/moyeothon/Team11_TwoAmpersandStar/global/jwt/exception/CustomAuthenticationException.java
+++
@@ -1,8 +0,0 @@
-package moyeothon.Team11_TwoAmpersandStar.global.jwt.exception;
-
-import org.springframework.security.core.AuthenticationException;
-public class CustomAuthenticationException extends AuthenticationException {
-    public CustomAuthenticationException(String message) {
-        super(message);
-    }
-}
