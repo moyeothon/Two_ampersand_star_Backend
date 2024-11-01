fa4cb89b9b53d7248e9427816af55082c357de2e
GamJaDo <kim0615157@gmail.com>
2024-11-01 15:15:01 +0900

    chore

--- src/main/java/moyeothon/Team11_TwoAmpersandStar/runningInfo/api/RunningInfoController.java
+++ src/main/java/moyeothon/Team11_TwoAmpersandStar/runningInfo/api/RunningInfoController.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/running/info")
 public class RunningInfoController {
 
-    ResponseEntity<>
+    // ResponseEntity<>
 }
